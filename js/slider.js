046f06dce821c048936c8a753def99764322b63f
MBellJDD <marybelljd@gmail.com>
2023-01-09 12:39:10 +0100

    js application to slider

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+// Creo una función que se llame asi misma para protejer el código y no mexclarlo con los demñas archivos de js
+(function () {
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+
+    let value;
+
+    buttonNext.addEventListener('click', () => {
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', () => {
+        changePosition(-1);
+    });
+
+    const changePosition = (add) => {
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value += add;
+
+        sliders[Number(currentTestimony) - 1].classList.remove('testimony__body--show');
+        if (value === sliders.length + 1 || value === 0) {
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        sliders[value - 1].classList.add('testimony__body--show');
+
+    }
+
+})();
